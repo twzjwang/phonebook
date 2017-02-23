020a844456e08723ac05bbc5b33e819c590f5236
twzjwang <twzjwang@gmail.com>
2017-02-23 22:12:14 +0800

    Modify some file for proper plot
    calculate.c :   Calculat average of append and findName time logged in
                    output.txt and opt.txt originally.
                    Add two file opt_bst.txt and opt_struct.txt in this commit.
    
    runtime.gp  :   This is a script for gnuplot.
                    Modify the command to plot four bars (change from two bars)
                    corresponded to four column calculated in output.txt.

--- calculate.c
+++ calculate.c
@@ -43,8 +43,51 @@ int main(void)
         opt_sum_a += opt_a;
         opt_sum_f += opt_f;
     }
-    fprintf(output, "append() %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0);
+    fclose(fp);
+
+    fp = fopen("opt_bst.txt", "r");
+    if (!fp) {
+        fp = fopen("orig.txt", "r");
+        if (!fp) {
+            printf("ERROR opening input file opt.txt\n");
+            exit(0);
+        }
+    }
+    double opt_bst_sum_a = 0.0, opt_bst_sum_f = 0.0, opt_bst_a, opt_bst_f;
+    for (i = 0; i < 100; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_bst_a, &opt_bst_f);
+        opt_bst_sum_a += opt_bst_a;
+        opt_bst_sum_f += opt_bst_f;
+    }
+    fclose(fp);
+
+    fp = fopen("opt_struct.txt", "r");
+    if (!fp) {
+        fp = fopen("orig.txt", "r");
+        if (!fp) {
+            printf("ERROR opening input file opt.txt\n");
+            exit(0);
+        }
+    }
+    double opt_struct_sum_a = 0.0, opt_struct_sum_f = 0.0, opt_struct_a, opt_struct_f;
+    for (i = 0; i < 100; i++) {
+        if (feof(fp)) {
+            printf("ERROR: You need 100 datum instead of %d\n", i);
+            printf("run 'make run' longer to get enough information\n\n");
+            exit(0);
+        }
+        fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_struct_a, &opt_struct_f);
+        opt_struct_sum_a += opt_struct_a;
+        opt_struct_sum_f += opt_struct_f;
+    }
+
+    fprintf(output, "append() %lf %lf %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0, opt_bst_sum_a / 100.0, opt_struct_sum_a / 100.0);
+    fprintf(output, "findName() %lf %lf %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0, opt_bst_sum_f / 100.0, opt_struct_sum_f / 100.0);
     fclose(output);
     fclose(fp);
     return 0;
--- scripts/runtime.gp
+++ scripts/runtime.gp
@@ -5,7 +5,11 @@ set title 'perfomance comparison'
 set term png enhanced font 'Verdana,10'
 set output 'runtime.png'
 
-plot [:][:0.150]'output.txt' using 2:xtic(1) with histogram title 'original', \
+plot [:][:0.100]'output.txt' using 2:xtic(1) with histogram title 'original', \
 '' using ($0-0.06):($2+0.001):2 with labels title ' ', \
 '' using 3:xtic(1) with histogram title 'optimized'  , \
-'' using ($0+0.3):($3+0.0015):3 with labels title ' '
+'' using ($0+0.3):($3+0.0015):3 with labels title ' ', \
+'' using 4:xtic(1) with histogram title 'opt bst'  , \
+'' using ($0+0.3):($4+0.0015):4 with labels title ' ', \
+'' using 5:xtic(1) with histogram title 'opt struct'  , \
+'' using ($0+0.3):($5+0.0015):5 with labels title ' '
